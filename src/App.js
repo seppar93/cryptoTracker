c2553fd275f4a8d9918dc03050218af338480cae
Sepehr Parirokh <sepehrparirokh@gmail.com>
2020-05-13 21:30:24 -0400

    fetching data setup and added test json to reduce use of api call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+// Modules
+import React, { Component } from 'react';
+
+// Styling
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Services
+import CryptoDataServices from './services/CryptoDataServices';
 
-export default App;
+export default class App extends Component {
+  constructor() {
+    super();
+    this.cryptoDataServices = new CryptoDataServices();
+  }
+
+
+  render() {
+    return <div>Testing</div>;
+  }
+}
---
+++ src/services/CryptoDataServices.js
@@ -0,0 +1,25 @@
+/**
+ * @class CryptoDataServices
+ * @description functions to fetch crypto data
+ */
+
+// Modules
+import axios from 'axios'
+
+export default class CryptoDataServices {
+  getData () {
+    const request = {
+      url:
+        'https://www.stackadapt.com/coinmarketcap/map'
+    };
+    return axios(request)
+  }
+
+  getCryptoPrice(data) {
+    const request = {
+      url: 'https://www.stackadapt.com/coinmarketcap/quotes',
+      params: { id: data },
+    };
+    return axios(request)
+  }
+}
